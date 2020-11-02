1b070648e21a87a1bf39c19105cdff953877f138
yangtao <yangtao080304@163.com>
2020-11-02 09:46:29 +0800

    初始化项目目录结构

---
+++ app/main.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+requirejs.config({
+    baseUrl: '../',
+    paths: {
+        app: './app',
+        fabric:'./src/fabric',
+        three:'./src/three',
+        ui:'./src/ui'
+    }
+})
+
+alert(11);
